13e243a45ec381120f3cd3978e462ae9b77b99c4
jademon <j2python@gmail.com>
2023-07-16 00:19:54 +0900

    .

--- src/pages/Page2.js
+++ src/pages/Page2.js
@@ -1,10 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import Component from "../assets/Cotainter";
 
 const Page2 = () => {
+  const [first, setFirst] = useState(false);
+  const [second, setSecond] = useState(false);
+  const [third, setThird] = useState(false);
+  const [fourth, setFourth] = useState(false);
+  const [fifth, setFifth] = useState(false);
   return (
-    <Component>
-      <></>
+    <Component title={"자동 심장 충격기 사용법"}>
+      <div
+        className="py-1 itemList-item"
+        onMouseOver={() => setFirst(true)}
+        onMouseLeave={() => setFirst(false)}
+      >
+        <div
+          className={
+            "font-semibold text-lg py-1 center " + (first ? "hidden" : "block")
+          }
+        >
+          1. 전원켜기
+        </div>
+        <div className={first ? "block" : "hidden"}>
+          심폐소생술에 방해가 되지 않는 위치에 놓은 후 전원 단추를 누른다.
+        </div>
+      </div>
+      <div
+        className="py-1 itemList-item"
+        onMouseOver={() => setSecond(true)}
+        onMouseLeave={() => setSecond(false)}
+      >
+        <div
+          className={
+            "font-semibold text-lg py-1 center " + (second ? "hidden" : "block")
+          }
+        >
+          2. 두개의 패드 부착
+        </div>
+        <div className={second ? "block" : "hidden"}>
+          패드 하나는 오른쪽 빗장뼈 아래에 부착. 또 다른 하나는 왼쪽 젖꼭지
+          아래의 겨드랑이 중앙선에 부착
+        </div>
+      </div>
+      <div
+        className="py-1 itemList-item"
+        onMouseOver={() => setThird(true)}
+        onMouseLeave={() => setThird(false)}
+      >
+        <div
+          className={
+            "font-semibold text-lg py-1 center " + (third ? "hidden" : "block")
+          }
+        >
+          3 . 심장 리듬 분석
+        </div>
+        <div className={third ? "block" : "hidden"}>
+          “분석중”이라는 지시가 나오면 환자에게서 손을 뗀다.
+        </div>
+      </div>
+      <div
+        className="py-1 itemList-item"
+        onMouseOver={() => setFourth(true)}
+        onMouseLeave={() => setFourth(false)}
+      >
+        <div
+          className={
+            "font-semibold text-lg py-1 center " + (fourth ? "hidden" : "block")
+          }
+        >
+          4. 자동 심장 충격 시행
+        </div>
+        <div className={fourth ? "block" : "hidden"}>
+          깜빡이는 단추를 눌러 심장 충격을 시행한다. 심장 충격 단추를 누르기
+          전에는 반드시 다른사람이 환자에게서 떨어져 있는지 확인한다.
+        </div>
+      </div>
+      <div
+        className="py-1 itemList-item"
+        onMouseOver={() => setFifth(true)}
+        onMouseLeave={() => setFifth(false)}
+      >
+        <div
+          className={
+            "font-semibold text-lg py-1 center " + (fifth ? "hidden" : "block")
+          }
+        >
+          5. 즉시 심폐소생술 실시
+        </div>
+        <div className={fifth ? "block" : "hidden"}>
+          심장 충격 실시 뒤 즉시 가슴압박30회와 인공호흡 2회를 시작한다.
+        </div>
+      </div>
     </Component>
   );
 };
--- src/pages/Page3.js
+++ src/pages/Page3.js
@@ -1,10 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import Component from "../assets/Cotainter";
 
 const Page3 = () => {
+  const [first, setFirst] = useState(false);
+  const [second, setSecond] = useState(false);
+  const [third, setThird] = useState(false);
+  const [fourth, setFourth] = useState(false);
   return (
-    <div>
-      <h2>헤더</h2>
-    </div>
+    <Component title={"일반 상해"}>
+      <div
+        className="py-1 itemList-item"
+        onMouseOver={() => setFirst(true)}
+        onMouseLeave={() => setFirst(false)}
+      >
+        <div
+          className={
+            "font-semibold text-lg py-1 center " + (first ? "hidden" : "block")
+          }
+        >
+          휴식
+        </div>
+        <div className={first ? "block" : "hidden"}>
+          상해가 더 진행되지 않도록 한다. 방법 : 부상 부위의 사용을 멈춘다.
+        </div>
+      </div>
+      <div
+        className="py-1 itemList-item"
+        onMouseOver={() => setSecond(true)}
+        onMouseLeave={() => setSecond(false)}
+      >
+        <div
+          className={
+            "font-semibold text-lg py-1 center " + (second ? "hidden" : "block")
+          }
+        >
+          냉찜질
+        </div>
+        <div className={second ? "block" : "hidden"}>
+          증각적인 냉각은 손상된 혈관을 숭축, 내출혈을 막아준다, 방법 :
+          아이스팩은 피부에 직접 닿지 않게 천을 사용한다. 부상 후 3일간 한시간에
+          1번 10~20분이 넘지 않도록 실시한다.
+        </div>
+      </div>
+      <div
+        className="py-1 itemList-item"
+        onMouseOver={() => setThird(true)}
+        onMouseLeave={() => setThird(false)}
+      >
+        <div
+          className={
+            "font-semibold text-lg py-1 center " + (third ? "hidden" : "block")
+          }
+        >
+          압박
+        </div>
+        <div className={third ? "block" : "hidden"}>
+          부기를 줄이고 회복 시간을 단축한다 .방법 : 부드러운 붕대, 압박 보호대
+          등을 사용한다. 될 수 있는 대로 아이스팩과 함께 부상 부위를 완전히
+          감싸야 한다.
+        </div>
+      </div>
+      <div
+        className="py-1 itemList-item"
+        onMouseOver={() => setFourth(true)}
+        onMouseLeave={() => setFourth(false)}
+      >
+        <div
+          className={
+            "font-semibold text-lg py-1 center " + (fourth ? "hidden" : "block")
+          }
+        >
+          거상
+        </div>
+        <div className={fourth ? "block" : "hidden"}>
+          부기와 통증을 줄여준다. 방법 : 적당한 물체나 의자를 사용하여 부상
+          부위를 심장보다 높은 위치에 둔다.
+        </div>
+      </div>
+    </Component>
   );
 };
 
